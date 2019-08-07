5a96f0f0e304e7eddec9bc7ecfb23be90a3870c5
Megan Shaw <meganrose@Megans-MacBook-Air.local>
2019-08-07 14:07:46 -0400

    Adding server and Heroku code

---
+++ server.js
@@ -0,0 +1,16 @@
+// Sets package requirements
+var express = require('express')
+var app = express()
+var path = require('path')
+var bodyParser = require('body-parser');
+
+// Setting port to 5000 for heroku
+var PORT = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/public'));
+
+require('./routes/index.js')(app);
+
+app.listen(PORT, function(){
+	console.log("Server listening on: " + PORT);
+});
